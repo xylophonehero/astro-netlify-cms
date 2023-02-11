cca415cb3e997e70d57df9a7a162db42b925ea2b
xylophonehero <nick97000@gmail.com>
2023-02-11 10:24:43 +0000

    feat:add netlify cms config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
 import { defineConfig } from 'astro/config';
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          {
+            name: 'posts',
+            label: 'Blog Posts',
+            folder: 'src/pages/posts',
+            create: true,
+            delete: true,
+            fields: [
+              { name: 'title', widget: 'string', label: 'Post Title' },
+              { name: 'body', widget: 'markdown', label: 'Post Body' },
+            ],
+          },
+        ]
+      }
+    })
+  ]
+});
